4dc3f6f9dcd55d0a17a12c2ee6ee203bf8f7c358
Maxim <meizutelefonovich@gmail.com>
2024-09-30 18:18:44 +0300

    ArrayTasks

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-//task 1
+//Створити масив [ 1, 2, 3 ]. Додати до нього в кінець наступні значення : 6, 5 , 4. Після цього до нього же додати у початок значення: -3, -2, -1
 
 let arr = [1,2,3];
 arr.push(6,5,4);
@@ -7,7 +7,7 @@ console.log(arr);
 arr.unshift(-3,-2,-1);
 console.log(arr);
 
-//task 2
+//Створити масив [ 100, -273.15, 0,1, 2, 3, 4, 5, 6, 9, 10 ]. Видалити перші два та остані три елементи цього масиву.
 
 const arr = [100, -273.15, 0,1, 2, 3, 4, 5, 6, 9, 10];
 arr.splice(0, 2);
@@ -16,52 +16,51 @@ console.log(arr);
 arr.splice(arr.length - 3, 3);
 console.log(arr);
 
-//task 3
+// Створити масив [1, 2, 3, 4, 5]. За допомогою методу splice перетворити його на [1, 4, 5].
 
 let arr = [1, 2, 3, 4, 5];
 
 arr.splice(1,2);
 console.log(arr);
 
-//task 4
+//Створити масив [1, 2, 3, 4, 5]. За допомогою методу splice записати у новий масив елементи [2, 3, 4].
 
 let arr = [1, 2, 3, 4, 5];
 
 const safearr = arr.splice(1, 3);
 console.log(safearr);
 
-//task 5
+//Створити масив [1, 2, 3, 4, 5]. За допомогою методу splice перетворити його на масив [1, 2, 3, 'a', 'b', 'c', 4, 5].
 
 let arr = [1, 2, 3, 4, 5];
 
 arr.splice(3, 0, 'a', 'b', 'c');
 console.log(arr);
 
-//task 6
-
+//Створити масив [1, 2, 3, 4, 5]. За допомогою методу splice перетворити його на масив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
 let arr = [1, 2, 3, 4, 5];
 arr.splice(1, 0, 'a', 'b');
 arr.splice(arr.length - 1, 0,'c');
 arr.splice(arr.length - 0, 0, 'e');
 console.log(arr);
 
-//task 7
+//Створити масив [1, 2, 3, 4, 5]. За допомогою методу slice зробити копію масива зі значеннями [4, 5]
 
 let arr = [1, 2, 3, 4, 5];
 console.log(arr.slice(3, 5));
 
-//task 8
+//Створити масив ['first', 'second', 'third', 'fourth', 'last']. Зробити копію зі значеннями  ['second', 'third'].
 
 let arr = ['first', 'second', 'third', 'fourth', 'last'];
 console.log(arr.slice(1, 3));
 
-//task 9
+//Створити масив ['first', 'second', 'third', 'fourth', 'last']. Перетворити його на масив ['last', 'fourth', 'third', 'second', 'first']
 
 let arr = ['first', 'second', 'third', 'fourth', 'last'];
 arr.reverse();
 console.log(arr);
 
-//task 10
+//Створити масив [1, 2, [3, 4, [5]], 6, 7]. Створити на його основі масив [1, 2, 3, 4, 5, 6, 7]
 
 let arr = [1, 2, [3, 4, [5]], 6, 7];
 console.log( arr[2][2] );
@@ -69,18 +68,19 @@ console.log( arr[2][2] );
 const arrFlat = arr.flat(2);
 console.log(arrFlat);
 
-//task 11
+//Створити масив з довільними значенням та перевірити, чи є у ньому елемент, який є рядком 'secret string'.
+
 
 let arr = [true, 'hello', 'cat', false, 2, 5];
 console.log(arr.includes('secret string'));
 
-//task 12;
+//Створити масив ['first', 'second', 'third', 'fourth', 'last']. Створити новий масив методом concat якому передати наступні параметри: 1000, 2000, [true, false];
 
 let arr = ['first', 'second', 'third', 'fourth', 'last'];
 const arrConcat = arr.concat(1000, 2000, [true, false]);
 console.log(arrConcat);
 
-//Bonus task 1
+//Створити масив з довільними елементами. За допомогою циклу вивести всі елементи масиву послідовно в консоль
 
 function arrNumbers(scaleTake) {
     let arr = ['first', 'second', 'third', 'fourth', 'last'];
@@ -93,7 +93,7 @@ const number = +prompt('Type number from 1 to 5: ');
 const result = number;
 return arrNumbers(result);
 
-//bonus task 2
+//Створити масив з довільними елементами. За допомогою циклу вивести всі елементи масиву з парними індексами послідовно в консоль
 
 let arr = [12, 100, 25, 30, 58];
 for(i = 0; i < arr.length; i++) {
